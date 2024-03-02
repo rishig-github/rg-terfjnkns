b93c16706a43ce830427d8bc7e724e513df6e971
rishig-github <153149330+rishig-github@users.noreply.github.com>
2024-03-02 07:15:34 -0500

    Add files via upload

---
+++ ec21.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAQERBLOJ5OGQIVLGO"
+    secret_key = "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"  
+}
+resource "aws_instance" "myautoec2" {
+    ami = "ami-0c7217cdde317cfec"
+    instance_type = "t2.micro"
+}
